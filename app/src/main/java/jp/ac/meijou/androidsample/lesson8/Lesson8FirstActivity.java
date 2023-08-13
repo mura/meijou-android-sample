46f0dedaf10f49952203f07592f96ebd14dfac70
Yohei Murayama <muracchi@gmail.com>
2023-08-13 19:22:42 +0900

    Lesson8に値の受け渡しを実装

--- app/src/main/java/jp/ac/meijou/androidsample/lesson8/Lesson8FirstActivity.java
+++ app/src/main/java/jp/ac/meijou/androidsample/lesson8/Lesson8FirstActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Optional;
 
 import jp.ac.meijou.androidsample.databinding.ActivityLesson8FirstBinding;
 
@@ -14,6 +18,25 @@ import jp.ac.meijou.androidsample.databinding.ActivityLesson8FirstBinding;
 public class Lesson8FirstActivity extends AppCompatActivity {
 
     private ActivityLesson8FirstBinding binding;
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                switch (result.getResultCode()) {
+                    case RESULT_OK:
+                        Optional.ofNullable(result.getData())
+                                .map(data -> data.getStringExtra("ret"))
+                                .map(text -> "Result: " + text)
+                                .ifPresent(text -> binding.lesson8FirstText.setText(text));
+                        break;
+                    case RESULT_CANCELED:
+                        binding.lesson8FirstText.setText("Result: Canceled");
+                        break;
+                    default:
+                        binding.lesson8FirstText.setText("Result: Unknown(" + result.getResultCode() + ")");
+                        break;
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +45,30 @@ public class Lesson8FirstActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         // 明示的Intent
-        binding.lesson8Button1.setOnClickListener(view -> {
+        binding.lesson8FirstButton1.setOnClickListener(view -> {
             var intent = new Intent(this, Lesson8SecondActivity.class);
             startActivity(intent);
         });
 
         // 暗黙的Intent
-        binding.lesson8Button2.setOnClickListener(view -> {
+        binding.lesson8FirstButton2.setOnClickListener(view -> {
             var intent = new Intent();
             intent.setAction(Intent.ACTION_VIEW);
             intent.setData(Uri.parse("https://www.yahoo.co.jp"));
             startActivity(intent);
+        });
+
+        // 文字を送信
+        binding.lesson8FirstButton3.setOnClickListener(view -> {
+            var intent = new Intent(this, Lesson8SecondActivity.class);
+            intent.putExtra("text", binding.lesson8FirstEditText.getText().toString());
+            startActivity(intent);
+        });
+
+        // 結果を取得
+        binding.lesson8FirstButton4.setOnClickListener(view -> {
+            var intent = new Intent(this, Lesson8SecondActivity.class);
+            getActivityResult.launch(intent);
         });
     }
 }
--- app/src/main/java/jp/ac/meijou/androidsample/lesson8/Lesson8SecondActivity.java
+++ app/src/main/java/jp/ac/meijou/androidsample/lesson8/Lesson8SecondActivity.java
@@ -1,19 +1,40 @@
 package jp.ac.meijou.androidsample.lesson8;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import jp.ac.meijou.androidsample.R;
+import java.util.Optional;
+
+import jp.ac.meijou.androidsample.databinding.ActivityLesson8SecondBinding;
 
 /**
  * Lesson8: ほかのActivityへの画面遷移（遷移先）
  */
 public class Lesson8SecondActivity extends AppCompatActivity {
 
+    private ActivityLesson8SecondBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lesson8_second);
+        binding = ActivityLesson8SecondBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Optional.ofNullable(getIntent().getStringExtra("text"))
+                .ifPresent(text -> binding.lesson8SecondText.setText(text));
+
+        binding.lesson8SecondButton1.setOnClickListener(view -> {
+            var intent = new Intent();
+            intent.putExtra("ret", "OK");
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+
+        binding.lesson8SecondButton2.setOnClickListener(view -> {
+            setResult(RESULT_CANCELED);
+            finish();
+        });
     }
 }
